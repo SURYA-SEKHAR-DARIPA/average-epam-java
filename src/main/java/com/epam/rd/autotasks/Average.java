7011f241f7701b61598c699b33b1c442a1705d52
thejerome <thejerome@mail.ru>
2021-02-08 15:57:58 +0300

    Initial Commit

---
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -0,0 +1,12 @@
+package com.epam.rd.autotasks;
+
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Use Scanner methods to read input
+
+    }
+
+}
