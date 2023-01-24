104ad2026857655d541604acb3a8ba05c840ec41
Surya <suryasekhardaripa60461@gmail.com>
2023-01-24 12:06:50 +0530

    Average done

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -1,12 +1,31 @@
 package com.epam.rd.autotasks;
 
 import java.util.Scanner;
+import java.util.*;
 
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int temp=0;
+        int cal=0;
         // Use Scanner methods to read input
+        ArrayList u=new ArrayList<>();
+        while(true){
+            int a=scanner.nextInt();
+            if(a!=0){
+                u.add(a);
+                temp=temp+a;
+                cal++;
 
-    }
+            }
+            else{
+                break;
+            }
+        }
+
+    
+    System.out.println(temp/cal);
+    
+}
 
 }
